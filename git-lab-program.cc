7342f8d5c0f57acce4cc857dd8df4d934c0cc3c5
Benjamin Edwards <be070723@ohio.edu>
2024-09-05 16:06:30 -0400

    adding c++ code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Benjamin Edwards
+ *        Date: 9/5/2024
+ * Description: git lab program for class
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
